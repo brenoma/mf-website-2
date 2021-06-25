be18da7cb1056d4e9f973b8f932d5de5a04b4f3a
Breno Menezes <brenoma@live.com>
2021-06-25 15:44:37 -0300

    Initial commit

---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,43 @@
+import React, {useState} from 'react'
+import { HeroContainer, HeroBg, VideoBg, HeroContent, HeroH1, HeroP, HeroBtnWrapper, ArrowForward, ArrowRight } from './HeroElement'
+import {Button} from '../ButtonElement'
+import Video from '../../videos/video.mp4'
+
+const HeroSection = () => {
+    const [hover, setHover] = useState(false)
+
+    const onHover = () => {
+        setHover(!hover)
+    }
+
+    return (
+        <HeroContainer id='home'>
+            <HeroBg>
+                <VideoBg autoPlay loop muted src={Video} type='video/mp4' />
+            </HeroBg>
+            <HeroContent>
+                <HeroH1>Virtual Banking Made Easy</HeroH1>
+                <HeroP>
+                    Sign up for a new account today and recieve $240 in credit towards your next payment.
+                </HeroP>
+                <HeroBtnWrapper>
+                    <Button to='signup'
+                    onMouseEnter={onHover}
+                    onMouseLeave={onHover}
+                    primary='true'
+                    dark='true'
+                    smooth={true}
+                    duration={500}
+                    spy={true}
+                    exact='true'
+                    offset={-80}
+                    >
+                        Get started {hover ? <ArrowForward /> : <ArrowRight />}
+                    </Button>
+                </HeroBtnWrapper>
+            </HeroContent>
+        </HeroContainer>
+    )
+}
+
+export default HeroSection
